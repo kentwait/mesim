50c5a05bd6f49f762c7e761311f3fea89f11805c
Kent Kawashima <kent@Kents-MacBook-Pro.local>
2017-07-15 03:25:08 +0900

    Adds sum slicing function

--- utils/math.go
+++ utils/math.go
@@ -5,3 +5,11 @@ func DivMod(numerator, denominator int64) (quotient, remainder int64) {
 	remainder = numerator % denominator
 	return
 }
+
+func Sum(input ...float64) (sum float64) {
+	sum = 0
+	for _, i := range input {
+		sum += i
+	}
+	return
+}
