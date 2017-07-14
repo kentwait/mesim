5decf131cb5ccac9239903d7157bc8ea629b4140
Kent Kawashima <kent@Kents-MacBook-Pro.local>
2017-07-15 01:20:27 +0900

    Adds functions for multinomial sampling

--- main.go
+++ main.go
@@ -1,5 +1,74 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Multinomial draws a sample from a multinomial distribution.
+func Multinomial(n int64, p []float64) []int64 {
+	// If n * len(p) > 1000, uses concurrency
+	if n*int64(len(p)) > int64(1000) {
+		workers := 0
+		resultChan := make(chan []int64)
+
+		for n > 1000 {
+			go func() {
+				resultChan <- multinomial(1000, p)
+			}()
+			n -= 1000
+			workers++
+		}
+		go func() {
+			resultChan <- multinomial(n, p)
+		}()
+		workers++
+
+		result := make([]int64, len(p))
+		for i := 0; i < workers; i++ {
+			tmp := <-resultChan
+			for j := 0; j < len(result); j++ {
+				result[j] += tmp[j]
+			}
+		}
+		return result
+	} else {
+		return multinomial(n, p)
+	}
+}
+
+func multinomial(n int64, p []float64) []int64 {
+	result := make([]int64, len(p))
+	cumP := make([]float64, len(p))
+
+	// Create a cummulative distribution of p
+	cumP[0] = p[0]
+	for i := 1; i < len(p); i++ {
+		cumP[i] = cumP[i-1] + p[i]
+	}
+
+	for i := int64(0); i < n; i++ {
+		// Generate pseudorandom number
+		x := rand.Float64()
+
+		// for j; e := range cumP {
+		for j := 0; j < len(cumP); j++ {
+			if x < cumP[j] {
+				result[j]++
+				break
+			} else if x == 1.0 {
+				result[len(p)]++
+				break
+			}
+		}
+	}
+	return result
+}
+
 func main() {
-	panic("Not implemented")
+	n := int64(100)
+	p := []float64{0.5, 0.3, 0.2}
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Println(Multinomial(n, p))
 }
