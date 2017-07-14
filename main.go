c748871b1e6d45f476e4c7f27f5ef14234961b54
Kent Kawashima <kent@Kents-MacBook-Pro.local>
2017-07-15 03:25:18 +0900

    Adds Evolve functions

--- main.go
+++ main.go
@@ -4,14 +4,65 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 
+	"log"
 	sampler "mesim/sampler"
 )
 
+// Evolve
+func Evolve(ancArray *[]int64, rateMatrix [][]float64) {
+	for i, char := range *ancArray {
+		(*ancArray)[i] = sampler.MultinomialWhere(int64(1), rateMatrix[char], int64(1))[0]
+	}
+}
+
+func EvolveAlt(ancArray *[]int64, rateMatrix [][]float64) {
+	mu := float64(0)
+	for x, row := range rateMatrix {
+		for y, v := range row {
+			if x != y {
+				mu += v
+			}
+		}
+	}
+
+	mutCoords := sampler.PoissonMutCoords(mu, int64(len(*ancArray)), int64(1))
+	var tmpArray []int64
+	if len(mutCoords) > 0 {
+		for _, coords := range mutCoords {
+			tmpArray = append(tmpArray, (*ancArray)[coords[1]])
+		}
+		Evolve(&tmpArray, rateMatrix)
+		for i, coords := range mutCoords {
+			(*ancArray)[coords[0]] = tmpArray[i]
+		}
+	}
+
+}
+
 func main() {
+	start := time.Now()
 	rand.Seed(time.Now().UTC().UnixNano())
-	fmt.Println(sampler.PoissonMutCoords(0.0001, 10000, 10))
+	a := 0.999991
+	b := 0.000003
+	rateMatrix := [][]float64{
+		[]float64{a, b, b, b},
+		[]float64{b, a, b, b},
+		[]float64{b, b, a, b},
+		[]float64{b, b, b, a},
+	}
+	arr := make([]int64, 1e8)
+	// fmt.Println(arr)
+	Evolve(&arr, rateMatrix)
+	// fmt.Println(arr)
+	log.Printf("Time: %s", time.Since(start))
+
+	start = time.Now()
+	arr = make([]int64, 1e8)
+	// fmt.Println(arr)
+	EvolveAlt(&arr, rateMatrix)
+	// fmt.Println(arr)
+	log.Printf("Time: %s", time.Since(start))
 }
