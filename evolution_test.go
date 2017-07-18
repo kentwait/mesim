9f9b98b9ae12a56575aa1af49314d7b1eb47dc57
kent <kentkawashima@gmail.com>
2017-07-18 17:07:42 +0900

    Adds TestEvolveChar

--- evolution_test.go
+++ evolution_test.go
@@ -1,8 +1,27 @@
 package mesim
 
-import "testing"
-import "fmt"
+import (
+	"testing"
+)
 
+func TestEvolveChar(t *testing.T) {
+	ancChar := 0
+	rateMatrix := [][]float64{
+		[]float64{0.001, 0.333, 0.333, 0.333},
+		[]float64{0.333, 0.001, 0.333, 0.333},
+		[]float64{0.333, 0.333, 0.001, 0.333},
+		[]float64{0.333, 0.333, 0.333, 0.001},
+	}
+	newChar := ancChar
+	// EvolveChar for 10 rounds
+	for i := 0; i < 10; i++ {
+		newChar = EvolveChar(newChar, rateMatrix)
+	}
+	if ancChar == newChar {
+		t.Error("ancChar should not be equal to newChar")
+		t.Error(ancChar, newChar)
+	}
+}
 func TestEvolveExplicit(t *testing.T) {
 	ancArray := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0} // 10 0's
 	rateMatrix := [][]float64{
@@ -17,6 +36,7 @@ func TestEvolveExplicit(t *testing.T) {
 		evolvedArray[i] = ancArray[i]
 	}
 
+	// EvolveExplicit for 10 rounds
 	for i := 0; i < 10; i++ {
 		EvolveExplicit(&evolvedArray, rateMatrix)
 	}
@@ -28,7 +48,6 @@ func TestEvolveExplicit(t *testing.T) {
 	}
 	if diffCnt == 0 {
 		t.Error("ancArray should not be equal to evolvedArray")
-		fmt.Println(ancArray)
-		fmt.Println(evolvedArray)
+		t.Error(ancArray, evolvedArray)
 	}
 }
