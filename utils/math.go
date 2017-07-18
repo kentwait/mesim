4835c3ecad671193110f609d4b1fac92fd1aafcd
kent <kentkawashima@gmail.com>
2017-07-18 20:53:26 +0900

    Adds ColSum and ColMean for 2d arrays

--- utils/math.go
+++ utils/math.go
@@ -13,3 +13,36 @@ func Sum(input ...float64) (sum float64) {
 	}
 	return
 }
+
+func ColSum(matrix [][]float64) []float64 {
+	if len(matrix) < 1 {
+		panic("Matrix must have one or more rows")
+	} else {
+		if len(matrix[0]) < 1 {
+			panic("Matrix must have one or more columns")
+		}
+	}
+	colSum := make([]float64, len(matrix[0]))
+	for m := 0; m < len(matrix); m++ {
+		for n := 0; n < len(matrix[0]); n++ {
+			colSum[n] += matrix[m][n]
+		}
+	}
+	return colSum
+}
+
+func ColMean(matrix [][]float64) []float64 {
+	if len(matrix) < 1 {
+		panic("Matrix must have one or more rows")
+	} else {
+		if len(matrix[0]) < 1 {
+			panic("Matrix must have one or more columns")
+		}
+	}
+	colMean := make([]float64, len(matrix[0]))
+	sums := ColSum(matrix)
+	for i, sum := range sums {
+		colMean[i] = sum / float64(len(matrix))
+	}
+	return colMean
+}
