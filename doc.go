0c8d3afba645cee1a5d771b2ac7d441031281bea
kent <kentkawashima@gmail.com>
2017-07-19 18:06:01 +0900

    Adds mesim package documentation in doc.go

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright 2017 Kent Kawashima. All rights reserved.
+// Use of this source code is governed under the Apache license
+// that can be found in the LICENSE file
+
+/*
+Package mesim implements a library to simulate the molecular evolution of haploid organisms through replication, mutation, recombination and selection.
+
+*/
+package mesim
