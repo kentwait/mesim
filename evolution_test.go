c848cd494ce2bad2970844a6bf4244f8e9c0f72f
kent <kentkawashima@gmail.com>
2017-07-19 17:48:18 +0900

    Adds TestEvolveSeqSpaceConstPop test

--- evolution_test.go
+++ evolution_test.go
@@ -102,7 +102,7 @@ func TestSeqSpaceToFitSpace(t *testing.T) {
 		[]float64{1.0, 1.0, 1.0, 1.0},
 		[]float64{1.0, 1.0, 1.0, 1.0},
 	}
-	fitnessFunction := func(seq []int) (multSum float64) {
+	fitnessFunction := func(seq []int, fitnessMatrix [][]float64) (multSum float64) {
 		multSum = float64(1)
 		for i, char := range seq {
 			multSum *= float64(fitnessMatrix[i][char])
@@ -161,7 +161,7 @@ func TestReplicateSelect(t *testing.T) {
 		[]float64{1.0, 1.0, 1.0, 1.0},
 		[]float64{1.0, 1.0, 1.0, 1.0},
 	}
-	fitnessFunction := func(seq []int) (multSum float64) {
+	fitnessFunction := func(seq []int, fitnessMatrix [][]float64) (multSum float64) {
 		multSum = float64(1)
 		for i, char := range seq {
 			multSum *= float64(fitnessMatrix[i][char])
@@ -246,13 +246,61 @@ func TestRecombineSeqSpace(t *testing.T) {
 			evolvedSeqSpace[i][j] = ancSeqSpace[i][j]
 		}
 	}
-	fmt.Println("anc", ancSeqSpace)
+	// fmt.Println("anc", ancSeqSpace)
 	RecombineSeqSpace(&evolvedSeqSpace, r)
-	fmt.Println("evolved", evolvedSeqSpace)
+	// fmt.Println("evolved", evolvedSeqSpace)
 
 	// TODO
 	// Break up RecombineSeqSpace to test these components individually
 	// Test whether evolvedSeqSpace has same shape as ancSeqSpace
 	// Test if recombination occurred
 	// Test if given a set of breakpoints, function uses
+}
+
+func TestEvolveSeqSpaceConstPop(t *testing.T) {
+	ancSeqSpace := [][]int{
+		[]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		[]int{2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+		[]int{3, 3, 3, 3, 3, 3, 3, 3, 3, 3},
+	}
+	mutationRate := 0.1
+	recombinationRate := 0.1
+	rateMatrix := [][]float64{
+		[]float64{0.00, 0.34, 0.33, 0.33},
+		[]float64{0.33, 0.00, 0.34, 0.33},
+		[]float64{0.33, 0.33, 0.00, 0.34},
+		[]float64{0.34, 0.33, 0.33, 0.00},
+	}
+	fitnessMatrix := [][]float64{
+		[]float64{1.0, 1.0, 1.0, 1.0}, // Fitness of 1 at pos 0 is 2x baseline
+		[]float64{1.0, 1.0, 1.0, 1.0},
+		[]float64{1.0, 1.0, 1.0, 1.0},
+		[]float64{1.0, 1.0, 1.0, 1.0},
+		[]float64{1.0, 1.0, 1.0, 1.0},
+		[]float64{1.0, 1.0, 1.0, 1.0},
+		[]float64{1.0, 1.0, 1.0, 1.0},
+		[]float64{1.0, 1.0, 1.0, 1.0},
+		[]float64{1.0, 1.0, 1.0, 1.0},
+		[]float64{1.0, 1.0, 1.0, 1.0},
+	}
+	fitnessFunc := func(seq []int, fitnessMatrix [][]float64) (multSum float64) {
+		multSum = float64(1)
+		for i, char := range seq {
+			multSum *= float64(fitnessMatrix[i][char])
+		}
+		return
+	}
+	evolvedSeqSpace := make([][]int, len(ancSeqSpace))
+	for i := range ancSeqSpace {
+		evolvedSeqSpace[i] = make([]int, len(ancSeqSpace[i]))
+		for j := range ancSeqSpace[i] {
+			evolvedSeqSpace[i][j] = ancSeqSpace[i][j]
+		}
+	}
+
+	fmt.Println(evolvedSeqSpace)
+	EvolveSeqSpaceConstPop(&evolvedSeqSpace, mutationRate, recombinationRate, rateMatrix, fitnessMatrix, fitnessFunc)
+	fmt.Println(evolvedSeqSpace)
+
 }
