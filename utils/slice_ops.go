21b9cb1f430a12308b262258e8b92b72e30fd248
kent <kentkawashima@gmail.com>
2017-07-19 20:48:07 +0900

    Adds convenience functions for 2d slices

--- utils/slice_ops.go
+++ utils/slice_ops.go
@@ -8,6 +8,14 @@ func DeepCopyInts(s []int) []int {
 	return newCopy
 }
 
+func DeepCopyInts2d(s [][]int) [][]int {
+	newCopy := make([][]int, len(s))
+	for i := range newCopy {
+		newCopy[i] = DeepCopyInts(s[i])
+	}
+	return newCopy
+}
+
 func CompareIntSlices(slice1, slice2 []int) (same bool, diffCoords []int) {
 	for i := range slice1 {
 		if slice1[i] != slice2[i] {
@@ -15,5 +23,21 @@ func CompareIntSlices(slice1, slice2 []int) (same bool, diffCoords []int) {
 			same = false
 		}
 	}
+	return
+}
+
+func CompareIntMatrix(matrix1, matrix2 [][]int) (same bool, diffCoords [][]int) {
+	var diffCoordsX, diffCoordsY []int
+
+	for i, row := range matrix1 {
+		for j := range row {
+			if matrix1[i][j] != matrix2[i][j] {
+				diffCoordsX = append(diffCoordsX, i)
+				diffCoordsY = append(diffCoordsY, j)
+				same = false
+			}
+		}
+	}
+	diffCoords = [][]int{diffCoordsX, diffCoordsY}
 	return
 }
